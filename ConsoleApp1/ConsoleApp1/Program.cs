51032cb069c266c97b2522d560b17eaba0ae2025
RohitKokare810 <Rohitkokare810@gmail.com>
2023-11-21 15:07:43 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class BankAccount
+{
+    // Properties
+    public string AccountNumber { get; }
+    public string AccountHolderName { get; set; }
+    private decimal _balance;
+
+    public decimal Balance
+    {
+        get { return _balance; }
+        private set { _balance = value; }
+    }
+
+    // Constructor
+    public BankAccount(string accountHolderName)
+    {
+        // Generate a simple account number (you might want to implement a more sophisticated method)
+        AccountNumber = GenerateAccountNumber();
+        AccountHolderName = accountHolderName;
+        Balance = 0;
+    }
+
+    // Methods
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited: {amount:C}. New balance: {Balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid deposit amount. Please deposit an amount greater than zero.");
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn: {amount:C}. New balance: {Balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
+
+    private string GenerateAccountNumber()
+    {
+        // This is a simple example; you might want to implement a more secure method for generating account numbers.
+        return Guid.NewGuid().ToString("N").Substring(0, 10).ToUpper();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of BankAccount
+        BankAccount myAccount = new BankAccount("John Doe");
+
+        // Print initial account details
+        Console.WriteLine($"Account Number: {myAccount.AccountNumber}");
+        Console.WriteLine($"Account Holder: {myAccount.AccountHolderName}");
+        Console.WriteLine($"Initial Balance: {myAccount.Balance:C}");
+
+        // Deposit some amount
+        myAccount.Deposit(1000);
+
+        // Withdraw some amount
+        myAccount.Withdraw(500);
+
+        // Print final account details
+        Console.WriteLine($"Final Balance: {myAccount.Balance:C}");
+
+        Console.ReadLine(); // Keep the console window open
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Product
+{
+    // Fields
+    public int pid;
+    public string pname;
+    public decimal pprice;
+
+    // Methods
+    public virtual void ShowDetails()
+    {
+        Console.WriteLine($"Product ID: {pid}");
+        Console.WriteLine($"Product Name: {pname}");
+        Console.WriteLine($"Product Price: {pprice:C}");
+    }
+
+    public virtual void SetInformation(int id, string name, decimal price)
+    {
+        pid = id;
+        pname = name;
+        pprice = price;
+    }
+}
+
+class HerbalProduct : Product
+{
+    // Additional fields for Herbal Product
+    public string herbsUsed;
+    public DateTime mfDate;
+    public DateTime expDate;
+
+    // Override methods to add details of Herbal Product fields
+    public override void ShowDetails()
+    {
+        base.ShowDetails(); // Call the base class method
+        Console.WriteLine($"Herbs Used: {herbsUsed}");
+        Console.WriteLine($"Manufacturing Date: {mfDate.ToShortDateString()}");
+        Console.WriteLine($"Expiration Date: {expDate.ToShortDateString()}");
+    }
+
+    public override void SetInformation(int id, string name, decimal price)
+    {
+        base.SetInformation(id, name, price); // Call the base class method
+        Console.Write("Enter Herbs Used: ");
+        herbsUsed = Console.ReadLine();
+        Console.Write("Enter Manufacturing Date (yyyy-mm-dd): ");
+        mfDate = DateTime.Parse(Console.ReadLine());
+        Console.Write("Enter Expiration Date (yyyy-mm-dd): ");
+        expDate = DateTime.Parse(Console.ReadLine());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an object of HerbalProduct class
+        HerbalProduct herbalProduct = new HerbalProduct();
+
+        // Set information for the herbal product
+        herbalProduct.SetInformation(1, "Herbal Shampoo", 15.99m);
+
+        // Display details of the herbal product
+        Console.WriteLine("\nHerbal Product Details:");
+        herbalProduct.ShowDetails();
+
+        Console.ReadLine(); // Keep the console window open
+    }
+}
